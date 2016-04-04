119ad0dfa2229f7658d788bc001c2867e2afa3bd
powerboat9 <powerboat9@users.noreply.github.com>
2016-04-04 00:01:14 -0400

    Finished doing some work.

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ from __future__ import division
 
 id = getID()
 
+#Used the pyaudio examples to write the code
 rate = float(16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=paInt16, rate=rate, output=True)
+stream = p.open(format=paInt16, channels=1, rate=rate, output=True)
 
 def playTone(frequency, duration):
     samplePerCycle = rate / frequency
+    data = ""
     for i in range(rate * duration):
         num = 32767 * math.sin(math.pi * 2 * (i % samplePerCycle) / samplePerCycle)
         data = data + char(int(num))
-    
+    stream.write(data)
+
+def close():
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
 def 
